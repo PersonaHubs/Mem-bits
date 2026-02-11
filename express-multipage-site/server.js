b4ef2f134c6955510188fffd4d85da931134ad30
PersonaHubs <mepdariguez@tip.edu.ph>
2026-02-11 14:42:20 +0800

    Finals Design

--- express-multipage-site/server.js
+++ express-multipage-site/server.js
@@ -13,6 +13,15 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'contact.html'));
+});
+
+
 app.get('/blog', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'blog.html'));
 });
